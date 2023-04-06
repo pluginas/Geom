9b5c8cd0c531e0291121d08377737af09b3f95dc
pluginas.24 <sokola18@yandex.ru>
2023-04-06 09:41:25 +0300

    windows work

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,32 @@
 package app;
 
-import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
-    @Override
-    public void accept(Event event) {
 
+    private final Window window;
+
+    // конструктор приложения
+    public Application() {
+        // создаём окно
+        window = App.makeWindow();
+        // задаём обработчиком событий текущий объект
+        window.setEventListener(this);
+        // делаем окно видимым
+        window.setVisible(true);
+    }
+
+
+    @Override
+    public void accept(Event e) {
+        // если событие - это закрытие окна
+        if (e instanceof EventWindowClose) {
+            // завершаем работу приложения
+            App.terminate();
+        } else if (e instanceof EventWindowCloseRequest) {
+            window.close();
+        }
     }
 }
