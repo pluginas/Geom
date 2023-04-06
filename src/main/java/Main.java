1ccc5c7e046b074da37efe34ca5dfce06103c1e9
pluginas.24 <sokola18@yandex.ru>
2023-04-06 09:57:40 +0300

    java doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 import app.Application;
 import io.github.humbleui.jwm.App;
 
+/**
+ * главный класс
+ */
 public class Main {
 
+    /**
+     * главный метод приложения
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         App.start(Application::new);
     }
--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,26 +4,49 @@ import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
+/**
+ * класс приложения
+ */
+
 public class Application implements Consumer<Event> {
 
+    /**
+     * окно приложения
+     */
     private final Window window;
 
-    // конструктор приложения
+    /**
+     * конструктор приложения
+      */
     public Application() {
-        // создаём окно
+        /**
+         * создаём окно
+         */
+
         window = App.makeWindow();
-        // задаём обработчиком событий текущий объект
+        /**
+         * задаём обработчиком событий текущий объект
+         */
         window.setEventListener(this);
-        // делаем окно видимым
+        /**
+         * делаем окно видимым
+         */
         window.setVisible(true);
     }
 
-
+    /**
+     * Обработчик событий
+     * @param e the input argument
+     */
     @Override
     public void accept(Event e) {
-        // если событие - это закрытие окна
+        /**
+         * если событие - это закрытие окна
+         */
         if (e instanceof EventWindowClose) {
-            // завершаем работу приложения
+            /**
+             * завершаем работу приложения
+             */
             App.terminate();
         } else if (e instanceof EventWindowCloseRequest) {
             window.close();
