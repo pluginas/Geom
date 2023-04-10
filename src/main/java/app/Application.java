64d2dac7439005b357bc3f1979024d8c83ffcda7
pluginas.24 <sokola18@yandex.ru>
2023-04-10 13:55:04 +0300

    window title ready

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -32,6 +32,14 @@ public class Application implements Consumer<Event> {
          * делаем окно видимым
          */
         window.setVisible(true);
+
+        window.setTitle("Java 2D");
+
+        // задаём размер окна
+        window.setWindowSize(900, 900);
+// задаём его положение
+        window.setWindowPosition(100, 100);
+
     }
 
     /**
