58191df26ebb7ec904ae427945746e495d078f8b
Geom <sokola18@yandex.ru>
2023-04-12 23:22:07 +0300

    multiline label

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -20,6 +20,11 @@ public class Colors {
      */
     public static final int PANEL_BACKGROUND_COLOR = Misc.getColor(32, 0, 0, 0);
 
+    /**
+     * Цвет текста
+     */
+    public static final int MULTILINE_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
+
 
     /**
      * Запрещённый конструктор
